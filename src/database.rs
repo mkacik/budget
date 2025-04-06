180460a2fb06395c672d88daa27e0bc2d5db5d5d
mkacik <m.m.kacik@gmail.com>
2025-04-05 21:29:59 -0500

    Create fallback for db path in prep for deployment

--- src/database.rs
+++ src/database.rs
@@ -8,11 +8,31 @@ const DATABASE_URL_ENV_VAR: &str = "DATABASE_URL";
 // All primary keys will be this type
 pub type ID = i32;
 
+fn get_fallback_database_url() -> String {
+  let current_dir = env::current_dir()
+    .expect("Cannot read current exectuable path, aborting!");
+
+  format!("sqlite:{}/budget.db", current_dir.display())
+}
+
 pub async fn get_db_pool() -> SqlitePool {
-    let database_url = env::var(DATABASE_URL_ENV_VAR).unwrap();
+    let database_url = match env::var(DATABASE_URL_ENV_VAR){ 
+      Ok(value) => value,
+      Err(_) => {
+        let fallback_database_url = get_fallback_database_url();
+        println!(
+          "{} env variable unset, falling back to {}.",
+          DATABASE_URL_ENV_VAR,
+          fallback_database_url,
+        );
+
+        fallback_database_url
+      },
+    };
+
     match SqlitePool::connect(&database_url).await {
         Ok(pool) => pool,
-        Err(_) => panic!("Could not create db connection pool, aborting!"),
+        Err(_) => panic!("Could not create db connection to {}, aborting!", database_url),
     }
 }
 
