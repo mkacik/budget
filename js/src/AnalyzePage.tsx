972fd39f17f555b7504810f22dc19c69293c1f5c
mkacik <m.m.kacik@gmail.com>
2025-12-24 12:40:54 -0600

    Make all headers fetch corresponding expenses on click in Analyze tab

--- js/src/AnalyzePage.tsx
+++ js/src/AnalyzePage.tsx
@@ -2,197 +2,11 @@ import React from "react";
 import { useState, useEffect } from "react";
 import { SpendingDataPoint } from "./types/SpendingData";
 
-import { BudgetView, BudgetCategoryView, BudgetItemView } from "./BudgetView";
+import { BudgetView } from "./BudgetView";
 import { MonthlySpendingData } from "./MonthlySpendingData";
+import { MonthlySpendingTable } from "./MonthlySpendingTable";
 import { ExpensesQuery, ExpensesList } from "./ExpensesList";
-import {
-  Col,
-  SmallInlineGlyph,
-  ErrorCard,
-  Section,
-  SectionHeader,
-  LoadingBanner,
-} from "./ui/Common";
-
-function getMonths(year: number): Array<string> {
-  return [...Array(12).keys()].map((m) => {
-    const month = String(m + 1).padStart(2, "0");
-    return `${year}-${month}`;
-  });
-}
-
-function SpendingTableCell({
-  obj,
-  spend,
-  onClick,
-}: {
-  obj: BudgetItemView | BudgetCategoryView | null;
-  spend: number;
-  onClick?: () => void;
-}) {
-  const amountPerMonth = obj === null ? 0 : obj.amountPerMonth;
-  const classNames = ["number", "r-align"];
-  if (spend <= 0) {
-    classNames.push("soft");
-  } else if (spend > amountPerMonth) {
-    classNames.push("red");
-  }
-
-  if (onClick !== undefined) {
-    classNames.push("td-button");
-  }
-
-  return (
-    <td className={classNames.join(" ")} onClick={onClick}>
-      {spend.toFixed(2)}
-    </td>
-  );
-}
-
-function MonthlySpendingTable({
-  year,
-  data,
-  budget,
-  updateExpensesQuery,
-}: {
-  year: number;
-  data: MonthlySpendingData | null;
-  budget: BudgetView;
-  updateExpensesQuery: (ExpensesQuery) => void;
-}) {
-  if (data === null) {
-    return null;
-  }
-
-  const months = getMonths(year);
-
-  const headerRow = months.map((month, idx) => {
-    const onClick = () => {
-      updateExpensesQuery({
-        variant: "period",
-        period: month,
-        categorySelector: "all-not-ignored",
-      } as ExpensesQuery);
-    };
-    return (
-      <th className="r-align nowrap td-button" key={idx} onClick={onClick}>
-        {month}
-      </th>
-    );
-  });
-
-  const rows: Array<React.ReactNode> = [];
-  for (const category of budget.categories) {
-    const cols: Array<React.ReactNode> = [<td key="hdr">{category.name}</td>];
-
-    for (const month of months) {
-      const spend = data.getCategorySpend(category.id, month);
-      const onClick = () => {
-        updateExpensesQuery({
-          variant: "period",
-          period: month,
-          categorySelector: category,
-        } as ExpensesQuery);
-      };
-      const cell = (
-        <SpendingTableCell
-          key={month}
-          obj={category}
-          spend={spend}
-          onClick={onClick}
-        />
-      );
-      cols.push(cell);
-    }
-
-    rows.push(
-      <tr key={`${category.id}`} className="bold highlight">
-        {cols}
-      </tr>,
-    );
-
-    for (const item of category.items) {
-      const cols: Array<React.ReactNode> = [
-        <td key="hdr" className="v-center">
-          <SmallInlineGlyph glyph="chevron_right" />
-          {item.name}
-        </td>,
-      ];
-
-      for (const month of months) {
-        const spend = data.getItemSpend(item.id, month);
-        const onClick = () => {
-          updateExpensesQuery({
-            variant: "period",
-            period: month,
-            categorySelector: item,
-          } as ExpensesQuery);
-        };
-        const cell = (
-          <SpendingTableCell
-            key={month}
-            obj={item}
-            spend={spend}
-            onClick={onClick}
-          />
-        );
-        cols.push(cell);
-      }
-
-      rows.push(<tr key={`${category.id}-${item.id}`}>{cols}</tr>);
-    }
-  }
-
-  const colsUncategorized: Array<React.ReactNode> = [
-    <td key="hdr">
-      <i>uncategorized</i>
-    </td>,
-  ];
-  for (const month of months) {
-    const spend = data.getUncategorizedSpend(month);
-    const onClick = () => {
-      updateExpensesQuery({
-        variant: "period",
-        period: month,
-        categorySelector: "uncategorized",
-      } as ExpensesQuery);
-    };
-    colsUncategorized.push(
-      <SpendingTableCell
-        key={month}
-        obj={null}
-        spend={spend}
-        onClick={onClick}
-      />,
-    );
-  }
-
-  rows.push(
-    <tr key={"__uncategorized"} className="bold highlight">
-      {colsUncategorized}
-    </tr>,
-  );
-
-  return (
-    <table>
-      <colgroup>
-        <Col />
-        {months.map((month) => (
-          <Col key={month} widthPct={7} />
-        ))}
-      </colgroup>
-
-      <thead>
-        <tr>
-          <th>Name</th>
-          {headerRow}
-        </tr>
-      </thead>
-
-      <tbody>{rows}</tbody>
-    </table>
-  );
-}
+import { ErrorCard, Section, SectionHeader, LoadingBanner } from "./ui/Common";
 
 function ExpensesSectionHeader({ query }: { query: ExpensesQuery | null }) {
   if (query === null) {
@@ -262,12 +76,14 @@ export function AnalyzePage({ budget }: { budget: BudgetView }) {
       <Section>
         <SectionHeader>Analyze spending</SectionHeader>
         <ErrorCard message={error} />
-        <MonthlySpendingTable
-          year={2025}
-          data={spendingData}
-          budget={budget}
-          updateExpensesQuery={(query) => setExpensesQuery(query)}
-        />
+        {spendingData && (
+          <MonthlySpendingTable
+            year={2025}
+            data={spendingData}
+            budget={budget}
+            updateExpensesQuery={(query) => setExpensesQuery(query)}
+          />
+        )}
       </Section>
       <Section>
         <ExpensesSectionHeader query={expensesQuery} />
---
+++ js/src/MonthlySpendingTable.tsx
@@ -0,0 +1,254 @@
+import React from "react";
+
+import { BudgetView, BudgetCategoryView, BudgetItemView } from "./BudgetView";
+import { MonthlySpendingData } from "./MonthlySpendingData";
+import { ExpensesQuery } from "./ExpensesList";
+import { Col, SmallInlineGlyph } from "./ui/Common";
+
+function getMonths(year: number): Array<string> {
+  return [...Array(12).keys()].map((m) => {
+    const month = String(m + 1).padStart(2, "0");
+    return `${year}-${month}`;
+  });
+}
+
+function SpendingTableColgroup() {
+  // each monthly cell should take 7%, header cell with category names
+  // will take remaining 16%
+  return (
+    <colgroup>
+      <Col />
+      {Array(12).map((month) => (
+        <Col key={month} widthPct={7} />
+      ))}
+    </colgroup>
+  );
+}
+
+function SpendingTableCell({
+  obj,
+  spend,
+  onClick,
+}: {
+  obj: BudgetItemView | BudgetCategoryView | null;
+  spend: number;
+  onClick?: () => void;
+}) {
+  const amountPerMonth = obj === null ? 0 : obj.amountPerMonth;
+  const classNames = ["number", "r-align"];
+  if (spend <= 0) {
+    classNames.push("soft");
+  } else if (spend > amountPerMonth) {
+    classNames.push("red");
+  }
+
+  if (onClick !== undefined) {
+    classNames.push("td-button");
+  }
+
+  return (
+    <td className={classNames.join(" ")} onClick={onClick}>
+      {spend.toFixed(2)}
+    </td>
+  );
+}
+
+function SpendingTableHeaderRow({
+  year,
+  updateExpensesQuery,
+}: {
+  year: number;
+  updateExpensesQuery: (ExpensesQuery) => void;
+}) {
+  const cells: Array<React.ReactNode> = [];
+  for (const month of getMonths(year)) {
+    const onClick = () => {
+      updateExpensesQuery({
+        variant: "period",
+        period: month,
+        categorySelector: "all-not-ignored",
+      } as ExpensesQuery);
+    };
+    const cell = (
+      <th key={month} className="r-align nowrap td-button" onClick={onClick}>
+        {month}
+      </th>
+    );
+    cells.push(cell);
+  }
+
+  return (
+    <tr>
+      <th>Name</th>
+      {cells}
+    </tr>
+  );
+}
+
+function SpendingTableUncategorizedRow({
+  year,
+  data,
+  updateExpensesQuery,
+}: {
+  year: number;
+  data: MonthlySpendingData;
+  updateExpensesQuery: (ExpensesQuery) => void;
+}) {
+  const headerCellOnClick = () => {
+    updateExpensesQuery({
+      variant: "period",
+      period: year.toString(),
+      categorySelector: "uncategorized",
+    } as ExpensesQuery);
+  };
+  const headerCell = (
+    <td className="td-button" onClick={headerCellOnClick}>
+      <i>uncategorized</i>
+    </td>
+  );
+
+  const cells: Array<React.ReactNode> = [];
+  for (const month of getMonths(year)) {
+    const spend = data.getUncategorizedSpend(month);
+    const onClick = () => {
+      updateExpensesQuery({
+        variant: "period",
+        period: month,
+        categorySelector: "uncategorized",
+      } as ExpensesQuery);
+    };
+    const cell = (
+      <SpendingTableCell
+        key={month}
+        obj={null}
+        spend={spend}
+        onClick={onClick}
+      />
+    );
+    cells.push(cell);
+  }
+
+  return (
+    <tr className="bold highlight">
+      {headerCell}
+      {cells}
+    </tr>
+  );
+}
+
+function SpendingTableRow({
+  obj,
+  year,
+  data,
+  updateExpensesQuery,
+}: {
+  obj: BudgetItemView | BudgetCategoryView;
+  year: number;
+  data: MonthlySpendingData;
+  updateExpensesQuery: (ExpensesQuery) => void;
+}) {
+  const isCategory = obj instanceof BudgetCategoryView;
+
+  const headerCellOnClick = () => {
+    updateExpensesQuery({
+      variant: "period",
+      period: year.toString(),
+      categorySelector: obj,
+    } as ExpensesQuery);
+  };
+  const headerCell = (
+    <td className="v-center td-button" onClick={headerCellOnClick}>
+      {!isCategory && <SmallInlineGlyph glyph="chevron_right" />}
+      {obj.name}
+    </td>
+  );
+
+  const cells: Array<React.ReactNode> = [];
+  for (const month of getMonths(year)) {
+    const spend = isCategory
+      ? data.getCategorySpend(obj.id, month)
+      : data.getItemSpend(obj.id, month);
+    const onClick = () => {
+      updateExpensesQuery({
+        variant: "period",
+        period: month,
+        categorySelector: obj,
+      } as ExpensesQuery);
+    };
+    const cell = (
+      <SpendingTableCell
+        key={month}
+        obj={obj}
+        spend={spend}
+        onClick={onClick}
+      />
+    );
+    cells.push(cell);
+  }
+
+  return (
+    <tr className={isCategory ? "bold highlight" : ""}>
+      {headerCell}
+      {cells}
+    </tr>
+  );
+}
+
+export function MonthlySpendingTable({
+  year,
+  data,
+  budget,
+  updateExpensesQuery,
+}: {
+  year: number;
+  data: MonthlySpendingData;
+  budget: BudgetView;
+  updateExpensesQuery: (ExpensesQuery) => void;
+}) {
+  const rows: Array<React.ReactNode> = [];
+  for (const category of budget.categories) {
+    rows.push(
+      <SpendingTableRow
+        key={`${category.id}`}
+        obj={category}
+        year={year}
+        data={data}
+        updateExpensesQuery={updateExpensesQuery}
+      />,
+    );
+
+    for (const item of category.items) {
+      rows.push(
+        <SpendingTableRow
+          key={`${category.id}-${item.id}`}
+          obj={item}
+          year={year}
+          data={data}
+          updateExpensesQuery={updateExpensesQuery}
+        />,
+      );
+    }
+  }
+
+  return (
+    <table>
+      <SpendingTableColgroup />
+
+      <thead>
+        <SpendingTableHeaderRow
+          year={year}
+          updateExpensesQuery={updateExpensesQuery}
+        />
+      </thead>
+
+      <tbody>
+        {rows}
+        <SpendingTableUncategorizedRow
+          year={year}
+          data={data}
+          updateExpensesQuery={updateExpensesQuery}
+        />
+      </tbody>
+    </table>
+  );
+}
